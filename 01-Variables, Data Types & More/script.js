2741d8f3b825b17470df18e3ddf65e6b0c1d6a5d
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-28 06:51:05 +0200

    Javascript Fundamentals (Working With Variables, Data Types & More)

--- 01-Variables, Data Types & More/script.js
+++ 01-Variables, Data Types & More/script.js
@@ -1,6 +1,8 @@
 
-//Working With Variables
+//Working With Variables.
 
+
+/*
 let firstName = 'John';
 const lastName = 'Doe';
 let age = 30;
@@ -21,3 +23,44 @@ person.age = 22;
 person.email = 'khalifa@gmail.com';
 
 console.log(person);
+
+let a, b, c;
+
+const d = 10,
+    e = 20,
+    f = 30;
+
+console.log(d);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(e);
+console.log(f);*/
+
+
+// Working with DataTypes.
+
+/*
+//string
+let firstName = "John";
+console.log(typeof firstName)
+
+//int
+let number = 22;
+console.log(typeof number)
+let number2 = 22.22;
+console.log(typeof number2)
+
+//Boolean
+let rainy = true;
+console.log(typeof rainy)
+
+//object
+let unDeclairedVariable = null;
+console.log(typeof unDeclairedVariable);
+
+//symbol
+const id = Symbol('id');
+console.log(typeof id);*/
+
+
