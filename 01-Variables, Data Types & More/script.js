c81be84068cddb5899cbe9fa9325aa0afcb8674d
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-28 06:34:33 +0200

    Javascript Fundamentals (Working With Variables, Data Types & More)

--- 01-Variables, Data Types & More/script.js
+++ 01-Variables, Data Types & More/script.js
@@ -1,3 +1,6 @@
+
+//Working With Variables
+
 let firstName = 'John';
 const lastName = 'Doe';
 let age = 30;
@@ -9,3 +12,12 @@ let score = 1;
 if (score <= 10) {
     console.log(score);
 }
+
+const person = {
+    name: 'John',
+}
+person.name = 'Khalifa';
+person.age = 22;
+person.email = 'khalifa@gmail.com';
+
+console.log(person);
