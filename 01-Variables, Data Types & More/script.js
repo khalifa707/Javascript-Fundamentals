ed8596ef2d4fa24402c4d075f647d1b0c9cff412
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-28 06:28:55 +0200

    Javascript Fundamentals (Working With Variables, Data Types & More)

---
+++ 01-Variables, Data Types & More/script.js
@@ -0,0 +1,11 @@
+let firstName = 'John';
+const lastName = 'Doe';
+let age = 30;
+
+console.log(firstName + lastName);
+console.log(age);
+
+let score = 1;
+if (score <= 10) {
+    console.log(score);
+}
