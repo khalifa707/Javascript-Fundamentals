aafeedc3ff0b700d042738b333cdf1e4810b3249
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-30 05:59:06 +0200

    Javascript Fundamentals (Arrays)

--- 02-Arrays/script.js
+++ 02-Arrays/script.js
@@ -43,5 +43,27 @@ arr.shift();
 arr.reverse();
 // includes() - Check to see if something is in the array
 arr.includes(445);
+x = arr.includes(445);
+
+// indexOf() - Return the index of the first match
+x = arr.indexOf(28);
+
+// Return array as a string
+x = arr.toString();
+x = arr.join();
+
+// slice() returns selected elements in an array, as a new array. Slice takes in the index of the first element and the index of the last element to be included in the new array.
+x = arr.slice(1, 4);
+
+// splice() works like slice() except it takes the index of the first element and the number of elements after that as a second argument. it also mutates the original array where slice() does not
+x = arr.splice(1, 4);
+
+// Remove a single element/value - The following will mutate the original array by taking out the element with the index of 4. x will be equal to a new array with that plucked out value.
+x = arr.splice(4, 1);
+
+// Chaining methods - Some methods can be chained depending on the return value.
+x = arr.slice(1, 4).reverse().toString().charAt(0);
+
+console.log(x);
 
 console.log(arr);
