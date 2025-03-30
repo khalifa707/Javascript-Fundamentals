0b4e64120e34560d6012fc865c003d579a4b95cf
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-30 05:48:35 +0200

    Javascript Fundamentals (Arrays)

--- 02-Arrays/script.js
+++ 02-Arrays/script.js
@@ -29,3 +29,9 @@ fruits[fruits.length] = 'peach';
 x = fruits;
 
 console.log(x);*/
+
+const arr = [28, 38, 44, 29, 109];
+// push() - Push a value on to the end of the array
+arr.push(100);
+
+console.log(arr);
