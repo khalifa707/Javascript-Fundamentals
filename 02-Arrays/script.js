d7e910e12fb942888d8ae67027e10aef5a1f6a7b
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-03-30 05:43:28 +0200

    Javascript Fundamentals (Arrays)

--- 02-Arrays/script.js
+++ 02-Arrays/script.js
@@ -5,3 +5,27 @@ const mixed = [12, 'Hello', true, null];
 
 console.log(mixed);
 console.log(numbers);
+
+// Get value by index
+x = numbers[0];
+
+x = numbers[0] + numbers[3];
+
+x = `My favorite fruit is an ${fruits[2]}`;
+
+x = numbers.length;
+
+fruits[2] = 'pear';
+
+// length is not read-only
+// fruits.length = 2;
+
+fruits[3] = 'strawberry';
+
+// Using the length as the index will always add on the the end
+fruits[fruits.length] = 'blueberry';
+fruits[fruits.length] = 'peach';
+
+x = fruits;
+
+console.log(x);
